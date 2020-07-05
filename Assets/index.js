1877ea0422521503181e94705d60ce99c4da561e
Raff1988 <raffaquat321@hotmail.co.uk>
2020-07-05 20:26:43 +0000

    message

---
+++ Assets/index.js
@@ -0,0 +1,11 @@
+    let score = 80,
+    if (score >= 90) {
+        console.log("YOU GET AN A")
+    }   
+
+    let randNum = Math.random();
+    if (randNum < 0.5) {
+        console.log("HEADS!")
+    }   else {
+        console.log("TAILS!")  
+    }
