1dfdfdbfba973c00e37c44394273c2282a1fce9f
Raff1988 <raffaquat321@hotmail.co.uk>
2020-07-13 20:50:40 +0000

    message

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,42 @@ let clickedCard = null;
 function onCardClicked(e) {
     const target = e.currentTarget;
 
+    if (
+    target === clickedCard || 
+    target.className.includes('done')
+    ) {
+        return;
+    }
+    
     target.className = target.className
     .replace('color-hidden', '')
     .trim();
-
-console.log(target.getAttribute('data-type'));
-
+    target.className += ' done';
 
     if (!clickedCard) {
 
     clickedCard = target;
     } else if (clickedCard) {
 
+    if (
+        clickedCard.getAttribute('data-color') !== 
+        target.getAttribute('data-color')
+        ) {
+            console.log(' cards not equal');
+            setTimeout(() => {
+
+            clickedCard.className = 
+            clickedCard.className.replace('done', '').trim() + 
+            ' color-hidden';
+            target.className =
+            target.className.replace('done', '').trim() + 
+            ' color-hidden';
+            clickedCard = null;
+            }, 750);
+        } else {
+            clickedCard = null;
+        }
+        clickedCard = null;
 }
 
 }
