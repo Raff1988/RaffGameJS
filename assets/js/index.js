768cce4f604078726b585acdc294d1e5372f20eb
Raff1988 <raffaquat321@hotmail.co.uk>
2020-07-19 15:27:07 +0000

    message

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
 let clickedCard = null;
 let preventClick = false;
 
+document.addEventListener('DOMContentLoaded', () => {
+    const timeLeftDisplay = document.querySelector('#time-left')
+    const startBtn = document.querySelector('#start-button')
+    timeLeft = 60
+
+    function countDown() {
+        setInterval(function(){
+            if (timeLeft <= 0) {
+                clearInterval(timeLeft = 0)
+            }
+            timeLeftDisplay.innerHTML = timeLeft
+            timeLeft -=1
+            }, 1000)
+            }
+            
+            startBtn.addEventListener('click', countDown)
+            
+        })
+
+
+
 
 function onCardClicked(e) {
 const target = e.currentTarget;
@@ -40,6 +61,11 @@ const target = e.currentTarget;
              }, 750);
         } else {
         clickedCard = null;
+
+
+
+
+        
         }   
     }
 }
